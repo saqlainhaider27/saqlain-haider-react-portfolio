f87cc8d22cea6efe82f176f8f098c2e5ac0b948a
Saqlain Haider <138958018+saqlainhaider27@users.noreply.github.com>
2025-04-22 20:27:16 +0500

    deploy

--- react-portfolio.client/vite.config.js
+++ react-portfolio.client/vite.config.js
@@ -39,6 +39,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: 'saqlain-haider-react-profile',
     plugins: [plugin()],
     resolve: {
         alias: {
@@ -58,4 +59,5 @@ export default defineConfig({
             cert: fs.readFileSync(certFilePath),
         }
     }
-})
+});
+
